f6da891d308c062ed3c4efee44ab002bb6a95e82
Felipe <luizfelipealbernaz@hotmail.com>
2018-04-02 22:10:22 -0300

    Binding UI to manager

---
+++ MySafe/MySafe/Managers/LoginManager.swift
@@ -0,0 +1,13 @@
+//
+//  LoginManager.swift
+//  MySafe
+//
+//  Created by Luiz Felipe Albernaz Pio on 02/04/18.
+//  Copyright © 2018 Luiz Felipe Albernaz Pio. All rights reserved.
+//
+
+import UIKit
+
+class LoginManager: NSObject {
+
+}
---
+++ MySafe/MySafe/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  MySafe
+//
+//  Created by Luiz Felipe Albernaz Pio on 02/04/18.
+//  Copyright © 2018 Luiz Felipe Albernaz Pio. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
