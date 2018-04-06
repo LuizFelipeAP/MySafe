78bf44bb7535286e8744165b3306021bb2503b66
Felipe <luizfelipealbernaz@hotmail.com>
2018-04-06 16:11:52 -0300

    Add Quick & Nimble as Tests dependencies

---
+++ MySafe/MySafeTests/AccountTests.swift
@@ -0,0 +1,22 @@
+//
+//  AccountTests.swift
+//  MySafeTests
+//
+//  Created by Luiz Felipe Albernaz Pio on 06/04/18.
+//  Copyright © 2018 Luiz Felipe Albernaz Pio. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import MySafe
+
+class AccountTests: QuickSpec {
+    
+    override func spec() {
+        
+        
+        
+    }
+    
+}
